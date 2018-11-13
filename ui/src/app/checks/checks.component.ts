057b767ec8dcd239856629f561a4ee403cbaa14e
Nick <nikage23+1@gmail.com>
2018-11-13 19:00:46 +0200

    add data table for checks list

--- ui/src/app/checks/checks.component.ts
+++ ui/src/app/checks/checks.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient }        from "@angular/common/http";
+import { map }               from "rxjs/operators";
+import { Observable }        from "rxjs";
 
 @Component({
   selector: 'app-checks',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChecksComponent implements OnInit {
 
-  constructor() { }
+  checks$: Observable<any[]>;
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
 
   ngOnInit() {
+    const apiV1Check = 'http://ec2-52-53-217-176.us-west-1.compute.amazonaws.com:31146/api/v1/check';
+    this.checks$ = this.http.get(apiV1Check).pipe(
+      map((checks: any[]) => checks.map(check => {
+        try {
+          const description = JSON.parse(check.description);
+          return {
+            ...check, description
+          }
+        } catch (e) {
+        //  is not json
+          return check;
+        }
+      }))
+    );
   }
+
+  isObject(val) {
+    return typeof val === 'object';
+  }
+
 
 }
--- ui/src/app/checks/checks.module.ts
+++ ui/src/app/checks/checks.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ChecksRoutingModule } from './checks-routing.module';
-import { ChecksComponent } from './checks.component';
+import { ChecksComponent }     from './checks.component';
+import { HttpClientModule }    from "@angular/common/http";
+import { MatCardModule }       from "@angular/material";
 
 @NgModule({
   declarations: [ChecksComponent],
   imports: [
     CommonModule,
-    ChecksRoutingModule
+    ChecksRoutingModule,
+    HttpClientModule,
+    MatCardModule,
   ],
 })
 export class ChecksModule { }
