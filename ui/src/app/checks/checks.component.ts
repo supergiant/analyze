47d82cf5d2a475314219badf16ec19b8e694a219
josephkane <josephckane@gmail.com>
2019-04-26 09:30:03 -0500

    Populate Checks Component
    * populate plugin checks with server data
    
    * will copy to settings component when approach is settled on

--- ui/src/app/checks/checks.component.ts
+++ ui/src/app/checks/checks.component.ts
@@ -1,9 +1,10 @@
-import { Component, AfterViewInit, ViewEncapsulation, ElementRef } from '@angular/core';
+import { Component, AfterViewInit, OnDestroy, ViewEncapsulation, ElementRef } from '@angular/core';
 import { HttpClient }                           from "@angular/common/http";
-import { map }                                  from "rxjs/operators";
-import { Observable }                           from "rxjs";
+import { map, takeUntil }                       from "rxjs/operators";
+import { Observable, Subject }                  from "rxjs";
 
 import { Plugin } from 'src/app/models/plugin';
+import { Check } from 'src/app/models/Check';
 import { PluginsService } from 'src/app/shared/services/plugins.service';
 import { CeRegisterService } from "../shared/services/ce-register.service";
 import { CustomElementsService } from "../shared/services/custom-elements.service";
@@ -15,10 +16,13 @@ import { CeCacheService } from "../shared/services/ce-cache.service";
   styleUrls: ['./checks.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class ChecksComponent implements AfterViewInit {
+export class ChecksComponent implements AfterViewInit, OnDestroy {
 
   private ceLoadedEvents$: Observable<CustomEvent>;
   private registeredCEs: Map<string, string>;
+  private readonly onDestroy = new Subject<void>();
+  private DOMWatcher: MutationObserver;
+  private container: string;
 
   constructor(
     private http: HttpClient,
@@ -27,20 +31,47 @@ export class ChecksComponent implements AfterViewInit {
     private customElService: CustomElementsService,
     private elRef: ElementRef,
     private ceCache: CeCacheService
-  ) { this.registeredCEs = ceCache.getAllRegisteredCEs(); }
+  ) {
+      this.registeredCEs = ceCache.getAllRegisteredCEs();
+      this.ceLoadedEvents$ = this.ceRegisterService.getAllCeLoadedEvents();
+    }
 
   ngAfterViewInit() {
-    const container = this.elRef.nativeElement.tagName.toLowerCase()
+    this.container = this.elRef.nativeElement.tagName.toLowerCase();
+
+    // TODO: how can we do this without DOM?
+    this.DOMWatcher = new MutationObserver(this.populateChecks.bind(this));
+    this.DOMWatcher.observe(document.querySelector(this.container), { attributes: false, childList: true, subtree: false })
 
     this.pluginsService.getAll().map((plugin: Plugin) => {
       const entrypoint = plugin.checkComponentEntryPoint;
 
-      if (!this.registeredCEs.has(entrypoint)) {
-        this.ceRegisterService.registerAndMountCe(entrypoint, container)
+      if (!this.registeredCEs.has(plugin.id)) {
+        this.ceRegisterService.registerAndMountCe(entrypoint, plugin.id, this.container);
       } else {
-        this.customElService.mountCustomElement(container, this.registeredCEs.get(entrypoint));
+        this.customElService.mountCustomElement(this.container, this.registeredCEs.get(plugin.id));
       }
-
     });
+  }
+
+  ngOnDestroy() {
+    this.onDestroy.next();
+    this.DOMWatcher.disconnect();
+  }
+
+  populateChecks(mutationsList, observer) {
+    // TODO: can we make this pure?
+    if (document.querySelector(this.container).childElementCount === this.pluginsService.getAll().length) {
+      this.pluginsService.getChecks().subscribe(
+        (checks: Check[]) => {
+          checks.forEach(c => {
+            const selector = this.registeredCEs.get(c.id);
+            const el = document.querySelector(selector);
+            el.setAttribute("check-result", JSON.stringify(c));
+          })
+        },
+        err => console.log(err)
+      )
+    }
   }
 }
--- ui/src/app/shared/services/ce-register.service.ts
+++ ui/src/app/shared/services/ce-register.service.ts
@@ -17,12 +17,6 @@ export class CeRegisterService {
     this.bus = document.querySelector<Element>('head');
 
     this.ceLoadedEvents$ = fromEventPattern(this.addHandler.bind(this), this.removeHandler.bind(this));
-
-    // this.test();
-  }
-
-  async test() {
-    await console.log(this.ceLoadedEvents$.toPromise);
   }
 
   private addHandler(handler) {
@@ -33,12 +27,12 @@ export class CeRegisterService {
     this.bus.removeEventListener(EventType.CE_LOADED_EVENT, handler);
   }
 
-  public registerAndMountCe(componentEntryPoint: string, containerSelector: string) {
+  public registerAndMountCe(componentEntryPoint: string, pluginId: string, containerSelector: string) {
     // TODO: this fn currently has too many responsibilities (register w/ DOM, cache, create and mount ce)
     // need to extract these into a synchronous workflow
     this.ceLoadedEvents$.subscribe((event: CustomEvent) => {
       const ceSelector = event.detail.selector;
-      this.ceCache.addRegisteredCE(componentEntryPoint, ceSelector)
+      this.ceCache.addRegisteredCE(pluginId, ceSelector)
       this.customElService.mountCustomElement(containerSelector, ceSelector)
     });
 
